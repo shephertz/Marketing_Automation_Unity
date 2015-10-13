fb7cdd3b188b454692a0ed3ad5d7d20a0cfc3d25
Akshay Mishra <akshay.mishra@shephertz.co.in>
2015-10-13 15:52:59 +0530

    Initial Commit.
    Signed-off-by: Akshay Mishra <akshay.mishra@shephertz.co.in>

---
+++ App42-MA-Unity3D-Sample/Assets/scripts/MA.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.app42Event;
+using System.Collections.Generic;
+
+public class MA : MonoBehaviour, App42CallBack {
+
+	EventService eventService = null;
+
+	public Text ResponseText;
+
+	// Use this for initialization
+	void Start () {
+		App42API.Initialize ("API_KEY","SECRET_KEY");
+		App42API.EnableEventService (true);
+		eventService = App42API.BuildEventService ();
+	}
+
+	/// <summary>
+	/// Tracks the event.
+	/// </summary>
+	/// <param name="IF">I.</param>
+	public void TrackEvent (InputField IF) {
+		String eventName = IF.text;
+		Dictionary<String,object> props = new Dictionary<string, object> ();
+		props.Add ("Level","First");
+		props.Add ("Difficulty","Normal");
+		eventService.TrackEvent (eventName, props, this);
+	}
+
+	public void OnSuccess (object response)
+	{
+		print ("Success : " + response.ToString());
+		ResponseText.color = Color.green;
+		ResponseText.text = "Success.";
+	}
+
+	public void OnException (Exception ex)
+	{
+		print ("Exception : " + ex.ToString());
+		ResponseText.color = Color.red;
+		ResponseText.text = "Error.";
+	}
+}
